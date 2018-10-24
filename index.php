b31632b9858656658ec37c8a0a599f7aa23aca31
Peerawit-SC <peerawit.sc@gmail.com>
2018-10-24 22:14:45 +0700

    change index.php

--- index.php
+++ index.php
@@ -1,21 +1,58 @@
 <?php
-require 'lib/nusoap.php';
+require_once "lib/nusoap.php";
+require_once "connectdb.php";
+$us = "urn:powerdata";
+$server = new soap_server();
+$server->configureWSDL('PowerData', $us);
+$server->wsdl->schemaTargerNamespace->$us;
+$server->soap_defencoding = 'utf-8';
 
-$server = new nusoap_server(); // Create a instance for nusoap server
-$server->configureWSDL("Soap Demo","urn:soapdemo"); // Configure WSDL file
+if(!isset($HTTP_RAW_POST_DATA)){
+    $HTTP_RAW_POST_DATA = file_get_contents('php://input');
+}
 
-// function hello
-function hello($name) {
-		return "Hello, $name";
-	}
+function getInfoAircon($lan){
+    $query = "SELECT * FROM data";
+    $result = mysqli_query($dbcon, $query);
+    
+    if($result != null){
+        $xml .= "<information>";
+        if($result){
+		    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+		        $xml .="<data>";
+    			$xml .= "<roomid>".$row['roomid']."</roomid>";
+    			$xml .= "<time>".$row['time']."</time>";
+    			$xml .= "<temperature>".$row['temperature']."</temperature>";
+    			$xml .= "<humidity>".$row['humidity']."</humidity>";
+    			$xml .="</data>";
+		    }
+        }else{
+            $xml .= "<error>No Data</error>";
+        }
+        $xml .="</information>";
+    }else{
+        $xml .="<error>Error".mysqli_error()."</error>";
+    }
+    $response = new soapval('return', 'xsd:string', $xml);
+    mysqli_close($dbcon);
+    return $response;
+}
+$server->register("getInfoAircon", array("getdata"=>"xsd:string"),
+				 				array("return"=>"xsd:string"), $us);
 
 
-$server->register("hello", // function hello
-    array("name" => "xsd:string"), // input type string
-    array("return" => "xsd:string"));  // output type string
+function insertAirData($roomid, $time, $temperature, $humidity){
+    $insert = "INSERT INTO data (roomid, time, temperature, humidity) VALUES ('$roomid', '$time', '$temperature', '$humidity')";
+	$result = mysqli_query($dbcon, $insert);
 
-// Run service
-$server->service(file_get_contents("php://input"));
+    return "data inserted";
+}
 
-?>
+$server->register("insertAirData", array("roomid" => "xsd:integer",
+                                         "time" => "xsd:string",
+                                         "temperature" => "xsd:float",
+                                         "humidity" => "xsd:float"),
+                                    array("return"=> "xsd:string"), $us);
 
+
+$server->service($HTTP_RAW_POST_DATA);
---
+++ service.php
@@ -0,0 +1,22 @@
+<?php
+require 'lib/nusoap.php';
+
+$server = new nusoap_server(); // Create a instance for nusoap server
+$server->configureWSDL("Soap Demo","urn:soapdemo"); // Configure WSDL file
+
+// function hello
+function hello($name) {
+		return "Hello, $name";
+		
+	}
+
+
+$server->register("hello", // function hello
+    array("name" => "xsd:string"), // input type string
+    array("return" => "xsd:string"));  // output type string
+
+// Run service
+$server->service(file_get_contents("php://input"));
+
+?>
+
